dcf83bf2e501f08c29647acd23a557ef4ce8b5f8
lnh24390-jpg <lnh24390@gmail.com>
2026-04-17 12:01:14 +0900

    Create 무지의 먹방_라이브.py

---
+++ 무지의 먹방_라이브.py
@@ -0,0 +1,25 @@
+import heapq
+
+
+def solution(food_times, k):
+    #전체 음식을 먹는 시간보다k가 크거나 같다면 -1
+    if sum(food_times)<=k:
+        return -1
+
+    q=[]
+    for i in range (len(food_times)):
+        heapq.heappush(q,(food_times[i],i+1))
+
+
+    sum_value=0
+    previous=0
+    length=len(food_times)
+
+    while sum_value+((q[0][0]-previous)*length)<=k:
+        now=heapq.heappop(q)[0]
+        sum_value+=(now-previous)*length
+        length-=1
+        previous=now
+
+    result=sorted(q,key=lambda x: x[1])
+    return result[(k-sum_value)%length][1]
